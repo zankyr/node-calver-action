c3081616d2a70dd044958e11a12ae36ca7dd232e
rik <p3mal88@gmail.com>
2023-03-09 15:54:37 +0100

    Add Calver

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const calver = require("calver");
+const { version } = require("./package.json");
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
+  const format = "yy.mm.dd";
+  const updatedVersion = calver.inc(format, "", "calendar");
+
+  console.log(`Current version: ${version}`);
+  console.log(`Updated version: ${updatedVersion}`);
+
+  core.setOutput("version", updatedVersion);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
