782ba2c003c38a7428032d086369d9ed012b8227
rik <p3mal88@gmail.com>
2023-03-09 17:28:41 +0100

    Fix json writing

--- src/workflow/actionWorkflow.js
+++ src/workflow/actionWorkflow.js
@@ -1,7 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const calver = require("calver");
-const jj = require("json");
 const packageJson = require("../../package.json");
 const fs = require("fs");
 
@@ -11,12 +10,16 @@ const actionWorkflow = () => {
     const updatedVersion = calver.inc(format, "", "calendar");
 
     console.log(`Current version: ${packageJson.version}`);
-    console.log(`Updated version: ${updatedVersion}`);
 
-    jj.core.setOutput("version", updatedVersion);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
+    packageJson.version = updatedVersion;
+    console.log(`Updated version: ${packageJson.version}`);
+
+    fs.writeFileSync(
+      "./package.json",
+      JSON.stringify(packageJson, undefined, 4)
+    );
+
+    core.setOutput("version", updatedVersion);
   } catch (error) {
     core.setFailed(error.message);
   }
